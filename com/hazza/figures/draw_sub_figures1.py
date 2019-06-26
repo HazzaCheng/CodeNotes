67488f4092640e2eb9df6f30b062387beb6aad57
HazzaCheng <hazzacheng@gmail.com>
2019-06-26 22:01:34 +0800

    update

--- com/hazza/figures/draw_sub_figures1.py
+++ com/hazza/figures/draw_sub_figures1.py
@@ -1,11 +1,18 @@
 import matplotlib
 import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from matplotlib.ticker import NullLocator
 
 matplotlib.rcParams['mathtext.default'] = 'regular'
 matplotlib.rcParams['font.family'] = 'Arial'
 
-plt.figure(figsize=(10, 8), dpi= 100)
-plt.subplots_adjust(wspace =0.5, hspace =0.5)
+#plt.figure(figsize=(30, 10), dpi= 100)
+
+fig, ax = plt.subplots(nrows=1, ncols=4, sharex=True, figsize=(24, 8), dpi=100)
+
+fig.text(0.5, 0.01, r'$\mathbf{Prior\ Self-Experience}$', fontsize=12, ha='center')
+
+plt.subplots_adjust(wspace =0.5, hspace=0.5)
 
 ratio = 0.025
 
@@ -13,17 +20,21 @@ ratio = 0.025
 y1, y2 = 8.9, 9.8
 ytotal = 12
 delta = ytotal * ratio
-ax1 = plt.subplot(221)
+ax1 = plt.subplot(141)
 ax1.spines['top'].set_visible(False)
 ax1.spines['right'].set_visible(False)
 # y轴
 plt.ylim((0, ytotal))
 plt.yticks((0, 3, 6, 9, 12))
-plt.ylabel(r'$\mathbf{\mathit{M}\ Target\ Act\ Score}$', fontsize=12)
+plt.ylabel(' ')
+plt.text(-1, 4.8, r"$\mathbf{\mathit{M}}$", fontsize=12, rotation=90, verticalalignment='bottom')
+plt.text(-1, 5.2, "Target Act Score", fontsize=10, rotation=90, verticalalignment='bottom')
+# plt.ylabel(r'$\mathbf{\mathit{\LARGE\mathrm K}}\ Target\ Act\ Score$', fontsize=12)
 # x轴
+plt.xlabel(' ')
 plt.xlim(-0.75, 1.75)
 x = ['Yes', 'No']
-plt.xlabel(r'$\mathbf{Prior\ self-experience}$', fontsize=12)
+
 # 小竖线1
 plt.axhline(y=y1 - delta, xmin=0.29, xmax=0.31, linewidth=1, color='black')
 plt.axhline(y=y1 + delta, xmin=0.29, xmax=0.31, linewidth=1, color='black')
@@ -37,24 +48,26 @@ plt.axhline(y=ytotal, xmin=0.3, xmax=0.7, linewidth=1, color = 'black')
 plt.axvline(x=0, ymin=(ytotal - delta)/ytotal, ymax=1, linewidth=1, color='black')
 plt.axvline(x=1, ymin=(ytotal - delta)/ytotal, ymax=1, linewidth=1, color='black')
 ax1.text(0.5, ytotal, '*', horizontalalignment='center', verticalalignment='bottom', fontsize=12)
-plt.bar(x, height=[y1, y2], color="silver", edgecolor="black", width=0.5, linewidth=1)
+plt.bar(x, height=[y1, y2], color=["silver", "white"], edgecolor=["black", "black"], width=0.7, linewidth=1)
 
 #图2
 
 y1, y2 = 2.45, 3.11
 ytotal = 4
 delta = ytotal * ratio
-ax1 = plt.subplot(222)
+ax1 = plt.subplot(142)
 ax1.spines['top'].set_visible(False)
 ax1.spines['right'].set_visible(False)
 # y轴
 plt.ylim((0, ytotal))
 plt.yticks((0, 1, 2, 3, 4))
-plt.ylabel(r'$\mathbf{\mathit{M}\ Serial\ Order\ Score}$', fontsize=12)
+plt.ylabel(' ')
+plt.text(-1, 1.6, r"$\mathbf{\mathit{M}}$", fontsize=12, rotation=90, verticalalignment='bottom')
+plt.text(-1, 1.73, "Serial Order Score", fontsize=10, rotation=90, verticalalignment='bottom')
 # x轴
 plt.xlim(-0.75, 1.75)
 x = ['Yes', 'No']
-plt.xlabel(r'$\mathbf{Prior\ self-experience}$', fontsize=12)
+
 # 小竖线1
 plt.axhline(y=y1 - delta, xmin=0.29, xmax=0.31, linewidth=1, color='black')
 plt.axhline(y=y1 + delta, xmin=0.29, xmax=0.31, linewidth=1, color='black')
@@ -68,24 +81,26 @@ plt.axhline(y=ytotal, xmin=0.3, xmax=0.7, linewidth=1, color = 'black')
 plt.axvline(x=0, ymin=(ytotal - delta)/ytotal, ymax=1, linewidth=1, color='black')
 plt.axvline(x=1, ymin=(ytotal - delta)/ytotal, ymax=1, linewidth=1, color='black')
 ax1.text(0.5, ytotal, '***', horizontalalignment='center', verticalalignment='bottom', fontsize=12)
-plt.bar(x, height=[y1, y2], color="silver", edgecolor="black", width=0.5, linewidth=1)
+plt.bar(x, height=[y1, y2], color=["silver", "white"], edgecolor=["black", "black"], width=0.7, linewidth=1)
 
 
 #图3
 y1, y2 = 2.15, 2.82
 ytotal = 4
 delta = ytotal * ratio
-ax1 = plt.subplot(223)
+ax1 = plt.subplot(143)
 ax1.spines['top'].set_visible(False)
 ax1.spines['right'].set_visible(False)
 # y轴
 plt.ylim((0, ytotal))
 plt.yticks((0, 1, 2, 3, 4))
-plt.ylabel(r'$\mathbf{\mathit{M}\ First\ Act\ Score}$', fontsize=12)
+plt.ylabel(' ')
+plt.text(-1, 1.6, r"$\mathbf{\mathit{M}}$", fontsize=12, rotation=90, verticalalignment='bottom')
+plt.text(-1, 1.73, "First Act Score", fontsize=10, rotation=90, verticalalignment='bottom')
 # x轴
 plt.xlim(-0.75, 1.75)
 x = ['Yes', 'No']
-plt.xlabel(r'$\mathbf{Prior\ self-experience}$', fontsize=12)
+
 # 小竖线1
 plt.axhline(y=y1 - delta, xmin=0.29, xmax=0.31, linewidth=1, color='black')
 plt.axhline(y=y1 + delta, xmin=0.29, xmax=0.31, linewidth=1, color='black')
@@ -99,7 +114,7 @@ plt.axhline(y=ytotal, xmin=0.3, xmax=0.7, linewidth=1, color = 'black')
 plt.axvline(x=0, ymin=(ytotal - delta)/ytotal, ymax=1, linewidth=1, color='black')
 plt.axvline(x=1, ymin=(ytotal - delta)/ytotal, ymax=1, linewidth=1, color='black')
 ax1.text(0.5, ytotal, '***', horizontalalignment='center', verticalalignment='bottom', fontsize=12)
-plt.bar(x, height=[y1, y2], color="silver", edgecolor="black", width=0.5, linewidth=1)
+plt.bar(x, height=[y1, y2], color=["silver", "white"], edgecolor=["black", "black"], width=0.7, linewidth=1)
 
 
 
@@ -107,17 +122,19 @@ plt.bar(x, height=[y1, y2], color="silver", edgecolor="black", width=0.5, linewi
 y1, y2 = 3.63, 3.42
 ytotal = 4
 delta = ytotal * ratio
-ax1 = plt.subplot(224)
+ax1 = plt.subplot(144)
 ax1.spines['top'].set_visible(False)
 ax1.spines['right'].set_visible(False)
 # y轴
 plt.ylim((0, ytotal))
 plt.yticks((0, 1, 2, 3, 4))
-plt.ylabel(r'$\mathbf{\mathit{M}\ Outcome\ Score}$', fontsize=12)
+plt.ylabel(' ')
+plt.text(-1, 1.6, r"$\mathbf{\mathit{M}}$", fontsize=12, rotation=90, verticalalignment='bottom')
+plt.text(-1, 1.73, "Outcome Score", fontsize=10, rotation=90, verticalalignment='bottom')
 # x轴
 plt.xlim(-0.75, 1.75)
 x = ['Yes', 'No']
-plt.xlabel(r'$\mathbf{Prior\ self-experience}$', fontsize=12)
+
 # 小竖线1
 plt.axhline(y=y1 - delta, xmin=0.29, xmax=0.31, linewidth=1, color='black')
 plt.axhline(y=y1 + delta, xmin=0.29, xmax=0.31, linewidth=1, color='black')
@@ -131,6 +148,6 @@ plt.axhline(y=ytotal, xmin=0.3, xmax=0.7, linewidth=1, color = 'black')
 plt.axvline(x=0, ymin=(ytotal - delta)/ytotal, ymax=1, linewidth=1, color='black')
 plt.axvline(x=1, ymin=(ytotal - delta)/ytotal, ymax=1, linewidth=1, color='black')
 ax1.text(0.5, ytotal + delta, r'$\it{ns}$', horizontalalignment='center', verticalalignment='bottom', fontsize=12)
-plt.bar(x, height=[y1, y2], color="silver", edgecolor="black", width=0.5, linewidth=1)
+plt.bar(x, height=[y1, y2], color=["silver", "white"], edgecolor=["black", "black"], width=0.7, linewidth=1)
 
 plt.show()
